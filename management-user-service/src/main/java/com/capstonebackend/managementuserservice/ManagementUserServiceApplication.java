14381f86ad80a48a488da1e9f64163bdd81d3c87
irfansettz <irfansettz@gmail.com>
2023-03-04 14:01:19 +0700

    +init project

---
+++ management-user-service/src/main/java/com/capstonebackend/managementuserservice/ManagementUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capstonebackend.managementuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementUserServiceApplication.class, args);
+    }
+
+}
---
+++ management-user-service/src/test/java/com/capstonebackend/managementuserservice/ManagementUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstonebackend.managementuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
