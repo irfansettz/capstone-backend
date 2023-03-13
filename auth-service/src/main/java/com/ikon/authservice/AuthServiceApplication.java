9f90ea175235119531111b5b9548eadd6521c92a
irfansettz <irfansettz@gmail.com>
2023-03-13 21:49:31 +0700

    ^fix

--- auth-service/src/main/java/com/ikon/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/ikon/authservice/AuthServiceApplication.java
@@ -4,10 +4,12 @@ import com.ikon.authservice.config.RSAKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(RSAKeyProperties.class)
+@EnableWebMvc
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
