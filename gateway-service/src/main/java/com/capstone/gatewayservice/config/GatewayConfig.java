0a3976699526c183f82cae9843ba58fd086ca698
irfansettz <irfansettz@gmail.com>
2023-03-14 13:32:42 +0700

    ^test

--- gateway-service/src/main/java/com/capstone/gatewayservice/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/capstone/gatewayservice/config/GatewayConfig.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ErrorHandler;
 
 @Configuration
 @RequiredArgsConstructor
@@ -14,28 +15,29 @@ public class GatewayConfig {
     private final AppFilter filter;
     @Bean
     RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
-        try {
-            return builder.routes()
-                    .route(r -> r.path("/v1/api/users/**")
-                            .and().method("GET", "POST", "UPDATE", "DELETE")
-                            .filters(f -> f.filter(filter))
-                            .uri("http://management-user-service:8082/"))
-                    .route(r -> r.path("/api/v1/auth/login")
-                            .and().method("GET", "POST", "UPDATE", "DELETE")
-                            .filters(f -> f.filter(filter))
-                            .uri("http://auth-service:8081/"))
-                    .route(r -> r.path("/api/v1/**")
-                            .and().method("GET", "POST", "UPDATE", "DELETE")
-                            .filters(f -> f.filter(filter))
-                            .uri("http://item-service:8083/"))
-                    .route(r -> r.path("/v1/api/**")
-                            .and().method("GET", "POST", "UPDATE", "DELETE")
-                            .filters(f -> f.filter(filter))
-                            .uri("http://request-service:8084/"))
-                    .build();
-        }catch (RuntimeException e){
-            System.out.println(e.getMessage());
-            throw new UnauthorizationException("Authorization failed");
-        }
+        return builder.routes()
+                .route(r -> r.path("/v1/api/users/**")
+                        .and().method("GET", "POST", "UPDATE", "DELETE")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://management-user-service:8082/"))
+                .route(r -> r.path("/api/v1/auth/login")
+                        .and().method("GET", "POST", "UPDATE", "DELETE")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://auth-service:8081/"))
+                .route(r -> r.path("/api/v1/**")
+                        .and().method("GET", "POST", "UPDATE", "DELETE")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://item-service:8083/"))
+                .route(r -> r.path("/v1/api/**")
+                        .and().method("GET", "POST", "UPDATE", "DELETE")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://request-service:8084/"))
+                .build();
     }
+
+    @Bean
+    public ErrorHandler errorHandler() {
+        throw new UnauthorizationException("Authorization failed");
+    }
+
 }
