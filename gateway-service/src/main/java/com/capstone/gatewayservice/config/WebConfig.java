0e95bff9dba0bdf4d4174cb7939a99134b124bd0
irfansettz <irfansettz@gmail.com>
2023-03-15 21:33:37 +0700

    +add cors

--- gateway-service/src/main/java/com/capstone/gatewayservice/config/WebConfig.java
+++ gateway-service/src/main/java/com/capstone/gatewayservice/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.capstone.gatewayservice.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@EnableAutoConfiguration(exclude = {WebFluxAutoConfiguration.class})
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
