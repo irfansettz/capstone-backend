199a3e5a938c765a5d70b91dcf597faa5c281b02
Nu'man Arif <35863852+akunumanarif@users.noreply.github.com>
2023-03-16 18:51:19 +0700

    CORS

--- gateway-service/src/main/java/com/capstone/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/capstone/gatewayservice/GatewayServiceApplication.java
@@ -2,10 +2,8 @@ package com.capstone.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation;
 
 @SpringBootApplication
-@ComponentScan("com.capstone.gatewayservice")
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
