518a98d57efb80e225755ca25b8e9cfead697430
M Nu'man Arif H <numanarif87@gmail.com>
2023-03-19 15:43:01 +0700

    Setup docker-compose with depended msg-brkr dpdncy

--- notification-service/src/main/java/com/capstonebackend/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/capstonebackend/notificationservice/NotificationServiceApplication.java
@@ -1,11 +1,7 @@
 package com.capstonebackend.notificationservice;
 
-import com.capstone.messagebrokerservice.RabbitMQMessageProducer;
-import com.capstonebackend.notificationservice.config.NotificationConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
 		scanBasePackages = {
@@ -19,19 +15,6 @@ public class NotificationServiceApplication {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
-//	    @Bean
-//		CommandLineRunner commandLineRunner(
-//            RabbitMQMessageProducer producer,
-//            NotificationConfig notificationConfig
-//            ) {
-//        return args -> {
-//            producer.publish(
-//                    new Person("Ali", 18),
-//                    notificationConfig.getInternalExchange(),
-//                    notificationConfig.getInternalNotificationRoutingKey());
-//        };
-//    }
-//
-//    record Person(String name, int age){}
+
 
 }
